97545596ae1d2315e6186c0d6a4ccd11d6e3b6f0
abircu <mdabircse4873@gmail.com>
2023-09-27 00:48:57 +0530

    data slicing and showing practice

--- src/components/FeaturesJobs/FeaturesJobs.jsx
+++ src/components/FeaturesJobs/FeaturesJobs.jsx
@@ -3,6 +3,9 @@ import Job from "../Job/Job";
 
 const FeaturesJobs = () => {
   const [jobs, setJobs] = useState([]);
+  //   this not the best way to load  show all data
+  const [dataLength, setDataLength] = useState(4);
+
   useEffect(() => {
     fetch("jobs.json")
       .then((res) => res.json())
@@ -10,18 +13,28 @@ const FeaturesJobs = () => {
   }, []);
 
   return (
-    <div className=" my-10">
-      <div className="text-center my-10">
+    <div className="mb-12">
+      <div className="text-center my-12">
         <h1 className="text-5xl font-bold">Featured Jobs:{jobs.length}</h1>
         <p className="text-xl">
           Explore thousands of job opportunities with all the information you
           need. Its your future
         </p>
       </div>
-      <div className=" grid grid-cols-1 md:grid-cols-2 gap-5">
-        {jobs.map((job) => (
+      <div className=" grid grid-cols-1 md:grid-cols-2 gap-6">
+        {jobs.slice(0, dataLength).map((job) => (
           <Job key={job.id} job={job} />
         ))}
+      </div>
+      <div className=" flex justify-center items-center mt-10 mb-10">
+        <div className={dataLength === jobs.length && "hidden"}>
+          <button
+            onClick={() => setDataLength(jobs.length)}
+            className="px-5 py-4 rounded-lg bg-indigo-600  font-extrabold text-white  hover:bg-[#7E90FE]"
+          >
+            Show All Jobs
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,3 +1,5 @@
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { AiFillDollarCircle } from "react-icons/ai";
 const Job = ({ job }) => {
   const {
     logo,
@@ -15,22 +17,34 @@ const Job = ({ job }) => {
           <img src={logo} alt="" />
         </div>
         <div className="card-body">
-          <h2 className="card-title">{job_title}</h2>
-          <p>{company_name}</p>
+          <h2 className="card-title text-2xl font-extrabold">{job_title}</h2>
+          <p className="text-[#757575] text-xl font-semibold">{company_name}</p>
           <div className="flex gap-4">
             <button className="px-3 py-2 rounded-lg border-solid border-2 border-indigo-600  font-extrabold text-[#7E90FE]">
               {remote_or_onsite}
             </button>
-            <button className="px-3 py-2 rounded-lg border-solid border-2 border-indigo-600 font-extrabold text-[#7E90FE]">
+            <button className="px-3 py-2 rounded-lg border-solid border-2 border-indigo-600 font-extrabold text-[#7E90FE] ">
               {job_type}
             </button>
           </div>
-          <div className="flex">
-            <p>{location}</p>
-            <p>{salary}</p>
+          <div className="flex gap-4">
+            <div className="flex justify-start items-center text-xl font-semibold text-[#757575]">
+              <p>
+                <HiOutlineLocationMarker></HiOutlineLocationMarker>
+              </p>
+              <p>{location}</p>
+            </div>
+            <div className="flex justify-start items-center text-xl font-semibold text-[#757575]">
+              <p>
+                <AiFillDollarCircle />
+              </p>
+              <p>{salary}</p>
+            </div>
           </div>
           <div className="card-actions">
-            <button className="btn btn-primary">view details</button>
+            <button className="btn btn-primary font-extrabold">
+              view details
+            </button>
           </div>
         </div>
       </div>
